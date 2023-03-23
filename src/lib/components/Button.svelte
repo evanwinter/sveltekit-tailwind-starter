e293693913d911ed79dda144b01739b87683106d
Evan Winter <evan@evanwinter.me>
2023-03-22 23:31:20 -0500

    chore: update readme and pass Button props through

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,5 +1,6 @@
 <button
-	class="text-sm bg-gray-200 px-2 py-1 rounded-md hover:text-white hover:bg-green-400 active:scale-95 transition-all"
+	{...$$restProps}
+	class="text-sm bg-gray-200 px-2 py-1 rounded-md hover:text-white hover:bg-green-400 active:scale-95 transition-all {$$restProps.class}"
 >
 	<slot />
 </button>
